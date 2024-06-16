20badee7ffffcad52673050c67c3573e2f4521b2
brandonerose <brandon.ed.rose@gmail.com>
2024-06-16 05:55:45 -0400

    Initial commit

---
+++ R/launch.R
@@ -0,0 +1,16 @@
+RosyPackages <- c("Rosyverse","RosyDev","RosyUtils","RosyDB","RosyREDCap")
+update_all <-function(){
+  was_updated_at_all <- F
+  for(p in RosyPackages){
+    version_before <- tryCatch(utils::packageVersion(p), error = function(e) NA)
+    remotes::install_github(paste0("brandonerose/",p))
+    version_after <- tryCatch(utils::packageVersion(p), error = function(e) NA)
+    if(!is.na(version_before)){
+      was_updated <- version_before != version_after
+    }else{
+      was_updated <- T
+    }
+    if(was_updated) was_updated_at_all <- T
+  }
+  if(was_updated_at_all) .rs.restartR()
+}

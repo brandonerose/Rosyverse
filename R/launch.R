4d7461c1b70bcb285fed492df5863075a69d1b35
brandonerose <brandon.ed.rose@gmail.com>
2026-05-23 10:37:13 -0500

    dev

--- R/launch.R
+++ R/launch.R
@@ -10,36 +10,43 @@ RosyPackages <- c(
   "RosyRx",
   "rosymap"
 )
-#' @title load_all
-#' @export
-load_all <-function(){
-  for(p in RosyPackages){
-    library(p,character.only = TRUE)
-  }
-}
 #' @title update_all
 #' @export
-update_all <-function(restart_after = TRUE){
+update_all <- function(restart_after = TRUE) {
   was_updated_at_all <- FALSE
-  for(p in RosyPackages){
-    version_before <- tryCatch(utils::packageVersion(p), error = function(e) NA)
-    repo <- "brandonerose"
-    if(p%in%c("REDCapSync","RosyREDCap"))repo <- "thecodingdocs"
-    remotes::install_github(paste0(repo,"/",p),upgrade = "never")
-    version_after <- tryCatch(utils::packageVersion(p), error = function(e) NA)
-    if(!is.na(version_before)){
-      was_updated <- version_before != version_after
-    }else{
-      was_updated <- TRUE
+  for (p in RosyPackages) {
+    version_before <- tryCatch(
+      utils::packageVersion(p),
+      error = function(e)
+        NA
+    )
+    was_updated <- TRUE
+    if (p == "REDCapSync") {
+      install.packages(p)
+    } else {
+      repo <- ifelse(p == "RosyREDCap", "thecodingdocs", "brandonerose")
+      remotes::install_github(paste0(repo, "/", p), upgrade = "always")
     }
-    if(was_updated) was_updated_at_all <- TRUE
+    version_after <- tryCatch(
+      utils::packageVersion(p),
+      error = function(e)
+        NA
+    )
+    if (!is.na(version_before)) {
+      was_updated <- version_before != version_after
+    }
+    if (was_updated)
+      was_updated_at_all <- TRUE
   }
-  if(was_updated_at_all&&restart_after) .rs.restartR()
+  if (was_updated_at_all && restart_after) {
+    .rs.restartR()
+  }
+  invisible()
 }
 #' @title remove_all
 #' @export
-remove_all <- function(){
-  for(x in c(RosyPackages)){
+remove_all <- function() {
+  for (x in c(RosyPackages)) {
     utils::remove.packages(x)
   }
   message("All Rosy packages removed!")

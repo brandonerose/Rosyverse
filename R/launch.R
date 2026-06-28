d0306d7214838e57496366070dcd1a863a5efa55
brandonerose <brandon.ed.rose@gmail.com>
2026-06-28 14:37:25 -0500

    RosyFinance

--- R/launch.R
+++ R/launch.R
@@ -2,6 +2,7 @@
 RosyPackages <- c(
   "Rosyverse",
   "RosyDev",
+  "RosyFinance",
   "RosyUtils",
   # "RosyDB",
   "RosyApp",
@@ -10,30 +11,41 @@ RosyPackages <- c(
   "RosyRx",
   "rosymap"
 )
+#' @title load_Rosyverse
+#' @export
+load_Rosyverse <-function(){
+  invisible(lapply(RosyPackages, library, character.only = TRUE))
+}
 #' @title update_all
 #' @export
 update_all <- function(restart_after = TRUE) {
   was_updated_at_all <- FALSE
+  new_versions <- old.packages()
   for (p in RosyPackages) {
     version_before <- tryCatch(
       utils::packageVersion(p),
       error = function(e)
         NA
     )
-    was_updated <- TRUE
+    was_updated <- FALSE
     if (p == "REDCapSync") {
-      install.packages(p)
+      if (p %in% new_versions[, "Package"]) {
+        install.packages(p)
+        was_updated <- TRUE
+      }
     } else {
       repo <- ifelse(p == "RosyREDCap", "thecodingdocs", "brandonerose")
       remotes::install_github(paste0(repo, "/", p), upgrade = "always")
-    }
-    version_after <- tryCatch(
-      utils::packageVersion(p),
-      error = function(e)
-        NA
-    )
-    if (!is.na(version_before)) {
-      was_updated <- version_before != version_after
+      version_after <- tryCatch(
+        utils::packageVersion(p),
+        error = function(e)
+          NA
+      )
+      if (is.na(version_before)) {
+        was_updated <- TRUE
+      } else{
+        was_updated <- version_before != version_after
+      }
     }
     if (was_updated)
       was_updated_at_all <- TRUE
@@ -46,8 +58,6 @@ update_all <- function(restart_after = TRUE) {
 #' @title remove_all
 #' @export
 remove_all <- function() {
-  for (x in c(RosyPackages)) {
-    utils::remove.packages(x)
-  }
+  utils::remove.packages(RosyPackages)
   message("All Rosy packages removed!")
 }
